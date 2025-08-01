6831bcda88416ca69ebe8cc8b76ed3dd7935a330
Osakpolor Obaseki <obasekiosa@gmail.com>
2025-08-01 09:19:11 +0100

    Update symbol list

--- mobile/locstream_rn/components/ui/IconSymbol.tsx
+++ mobile/locstream_rn/components/ui/IconSymbol.tsx
@@ -18,6 +18,9 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'gear': 'settings',
+  'mappin.and.ellipse': 'location-on', // emergency-share
+  'calendar': 'calendar-month'
 } as IconMapping;
 
 /**
